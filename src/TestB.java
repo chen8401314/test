563ec2b71f40a3d3703ab2cb50b4c8acd3ffbbee
chenxianggit <328692993@qq.com>
2016-12-15 15:52:28 +0800

    有修改

--- src/TestB.java
+++ src/TestB.java
@@ -1,6 +1,6 @@
 
 public class TestB {
 public static void main(String[] args) {
-	System.out.println(java.util.UUID.randomUUID().toString());
+	System.out.println(java.util.UUID.randomUUID().toString()+"ssds");
 }
 }
